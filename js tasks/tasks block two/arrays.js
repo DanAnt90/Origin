d4edbf374148571a4b6ea94b221d9a40fc1d0e4b
anton <danilovskiyanton@gmail.com>
2016-06-15 21:21:53 +0300

    class

--- js tasks/tasks block two/arrays.js
+++ js tasks/tasks block two/arrays.js
@@ -1,15 +1,16 @@
 var arr = [];
 var arr2 = [];
+var number = 10;
 
 function arrRandom(min, max) {
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < number; i++) {
         arr.push(Math.floor(Math.random() * (max - min)) + min)
     }
 }
 arrRandom(-100, 100);
 
 function arrContact(arr) {
-    for (var i in arr) {
+    for (var i = 0; i < arr.length; i++) {
         arr[i] = Math.pow(arr[i], 2);
         if ((arr[i] > 100)) {
             arr2.push(arr[i]);
--- js tasks/tasks block two/date.js
+++ js tasks/tasks block two/date.js
@@ -2,12 +2,11 @@ var date1 = new Date(2013, 0, 1);
 var date2 = new Date(2013, 0, 1);
 
 function date(date1, date2) {
-    date1 = date1.getTime();
-    date2 = date2.getTime();
-    if (date1 < date2) {
+
+    if (date1.getTime() < date2.getTime) {
         alert("первое событие было раньше");
 
-    } else if (date1 > date2) {
+    } else if (date1.getTime() > date2.getTime()) {
         alert("второе событие было раньше");
 
     } else {
