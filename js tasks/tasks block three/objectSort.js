4aa1d21d542d7173f6beb2a4138823860cf33331
Anton <core.safecrl>
2016-06-16 12:50:47 +0300

    block three

---
+++ js tasks/tasks block three/objectSort.js
@@ -0,0 +1,43 @@
+var bon = {
+    name: "Bon",
+    surname: "Scott",
+    weight: 70,
+    height: 180
+};
+var clifford = {
+    name: "Clifford",
+    surname: "Williams",
+    weight: 90,
+    height: 180
+};
+var brian = {
+    name: "Brian",
+    surname: "Johnson",
+    weight: 60,
+    height: 180
+};
+var chris = {
+    name: "Chris",
+    surname: "Slade",
+    weight: 100,
+    height: 180
+};
+
+var people = [ clifford , brian , chris , bon];
+
+
+function objSort(humanOne, humanTwo) {
+   if (humanOne.height - humanTwo.height > 0 || humanOne.height - humanTwo.height < 0){
+  return humanOne.height - humanTwo.height;
+    }
+    else {
+        if(humanOne.surname > humanTwo.surname){
+            return 1;
+        }
+        else{
+            return 0
+        }
+    }
+}
+
+people.sort(objSort);
