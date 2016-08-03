c6fe776ba4ea365e8fcc8f1c94ed790b9def0be6
anton <danilovskiyanton@gmail.com>
2016-08-03 21:24:54 +0300

    myProject 1

--- myProject/js/src/script.js
+++ myProject/js/src/script.js
@@ -1,6 +1,19 @@
 $(".servise_two_container").slick({
-  dots: false,
-  infinite: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
 });
+
+function hover(firstElem, secondElem) {
+    firstElem.on("mouseover", function(e) {
+        $(e.currentTarget).prev().children().css("opacity", "0.2");
+    })
+    secondElem.on("mouseover", function(e) {
+        $(e.currentTarget).children().css("opacity", "0.2")
+    })
+    secondElem.on("mouseout", function(e) {
+        $(e.currentTarget).children().css("opacity", "1")
+    })
+};
+hover($(".our_team_img_container_hover"), $(".our_team_img_container_main_image"));
