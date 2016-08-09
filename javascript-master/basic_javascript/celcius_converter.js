6cef60ad2cc3e56bf36e82a05f65f77ff9515ecc
Anton <core.safecrl>
2016-08-09 16:26:01 +0300

    project progress

--- javascript-master/basic_javascript/celcius_converter.js
+++
@@ -1,5 +0,0 @@
-function celciusToFahrenheit(celcius) {
-    return 9/5 * celcius + 32;
-}
-
-alert(celciusToFahrenheit(100));
--- js tasks/BOM/js.js
+++
@@ -1,6 +0,0 @@
-
-var myWindow= window.open( "http://www.w3schools.com/", "test", "width=200, height=200");
-x.resizeTo(500, 500);
-window.scrollTo(0, 0);
-document.write(navigator.userAgent);
-myWindow.close();
