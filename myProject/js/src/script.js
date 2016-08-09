364d25c669b4c3b0b6908bb061263dcf4fbb50ef
Anton <core.safecrl>
2016-08-09 15:51:58 +0300

    myPr

--- myProject/js/src/script.js
+++ myProject/js/src/script.js
@@ -4,22 +4,4 @@ $(".servise_two_container").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
 });
-/*$(".about_us_img").slick({
-    dots: false,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-});*/
-/*
-function hover(firstElem, secondElem) {
-    firstElem.on("mouseover", function(e) {
-        $(e.currentTarget).prev().children().css("opacity", "0.2");
-    })
-    secondElem.on("mouseover", function(e) {
-        $(e.currentTarget).children().css("opacity", "0.2")
-    })
-    secondElem.on("mouseout", function(e) {
-        $(e.currentTarget).children().css("opacity", "1")
-    })
-};
-hover($(".about_us_img_container_hover"), $(".about_us_img_container_main_image"));*/
+
